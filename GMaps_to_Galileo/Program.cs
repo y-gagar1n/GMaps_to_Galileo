d8f5c2ab4be9390aa434dab9599f9cefa5b4451c
Yury <y.timofeyev@gmail.com>
2014-06-28 14:09:12 +0400

    some new icons

--- GMaps_to_Galileo/Program.cs
+++ GMaps_to_Galileo/Program.cs
@@ -12,6 +12,7 @@ namespace GMaps_to_Galileo
         private static Dictionary<string, string> iconsMap = new Dictionary<string, string>()
         {
             {"icon-1085", "BookmarkStyle_6"},
+            {"icon-1081", "BookmarkStyle_6"},
             {"icon-1101", "BookmarkStyle_10"},
             {"icon-1143", "BookmarkStyle_21"},
             {"icon-1197", "BookmarkStyle_9"},
@@ -19,9 +20,12 @@ namespace GMaps_to_Galileo
             {"icon-1417", "BookmarkStyle_12"},
             {"icon-1423", "BookmarkStyle_40"},
             {"icon-1459", "BookmarkStyle_24"},
+            {"icon-1379", "BookmarkStyle_15"},
             {"icon-960-F4EB37", "BookmarkStyle_27"},
             {"icon-960-FF8277", "BookmarkStyle_0"},
-            {"icon-961-FF8277", "BookmarkStyle_20"}
+            {"icon-961-FF8277", "BookmarkStyle_20"},
+            {"icon-991","BookmarkStyle_14"},
+            {"icon-1165","BookmarkStyle_21"},
         };
 
         private static void Main(string[] args)
